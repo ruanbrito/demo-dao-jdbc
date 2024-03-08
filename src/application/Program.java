ccc604d23142347c7ede3a2a466be3cf347248b0
ruanbrito <rpablobrito@gmail.com>
2024-03-08 12:27:15 -0300

    Implementação do método findByDepartment da classe SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,24 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		Seller seller = sellerDao.findById(6);
-		
-		System.out.println(seller);
 
+		// Seller seller = sellerDao.findById(6);
+		Department depart = new Department(2, null);
+		List<Seller> listSeller = sellerDao.findByDepartment(depart);
+
+		for (Seller list : listSeller) {
+			System.out.println(list);
+		}
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -12,10 +15,10 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-public class SellerDaoJDBC implements SellerDao{
-	
+public class SellerDaoJDBC implements SellerDao {
+
 	private Connection conn;
-	
+
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
@@ -23,19 +26,19 @@ public class SellerDaoJDBC implements SellerDao{
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -43,27 +46,21 @@ public class SellerDaoJDBC implements SellerDao{
 		PreparedStatement pst = null;
 		ResultSet rs = null;
 		try {
-			pst = conn.prepareStatement(
-					"SELECT seller.*, department.Name as DepName "
-					+ "FROM seller "
-					+ "INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+"WHERE seller.Id = ?");
+			pst = conn.prepareStatement("SELECT seller.*, department.Name as DepName " + "FROM seller "
+					+ "INNER JOIN department " + "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
 			pst.setInt(1, id);
-			
+
 			rs = pst.executeQuery();
-			
-			if(rs.next()) {
-				Department depart = instatiateDepartment(rs);
+
+			if (rs.next()) {
+				Department depart = instantiateDepartment(rs);
 				Seller seller = instantiateSeller(rs, depart);
 				return seller;
 			}
 			return null;
-		} 
-		catch (SQLException e ) {
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 
 			DB.closeResultSet(rs);
 			DB.closeStatement(pst);
@@ -71,22 +68,22 @@ public class SellerDaoJDBC implements SellerDao{
 	}
 
 	private Seller instantiateSeller(ResultSet rs, Department depart) throws SQLException {
-		Seller seller = new  Seller();
+		Seller seller = new Seller();
 		seller.setId(rs.getInt("Id"));
 		seller.setName(rs.getString("Name"));
 		seller.setEmail(rs.getString("Email"));
 		seller.setBirthdate(rs.getDate("BirthDate"));
 		seller.setBaseSalary(rs.getDouble("BaseSalary"));
 		seller.setDepartment(depart);
-		
+
 		return seller;
 	}
 
-	private Department instatiateDepartment(ResultSet rs) throws SQLException {
-		Department  depart = new Department();
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department depart = new Department();
 		depart.setId(rs.getInt("DepartmentId"));
 		depart.setName(rs.getString("DepName"));
-		
+
 		return depart;
 	}
 
@@ -94,6 +91,43 @@ public class SellerDaoJDBC implements SellerDao{
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department depart) {
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		try {
+			pst = conn.prepareStatement("SELECT seller.*, department.Name as DepName FROM seller "
+					+ "INNER JOIN department ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			pst.setInt(1, depart.getId());
+
+			rs = pst.executeQuery();
+
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			while (rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId"));
+
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				Seller seller = instantiateSeller(rs, dep);
+				list.add(seller);
+			}
+			return list;
+
+		} 
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} 
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(pst);
+		}
 	}
 
 }
