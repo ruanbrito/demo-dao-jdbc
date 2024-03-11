c56053d50d76b771d70e85d0bdba0c74105c3790
ruanbrito <rpablobrito@gmail.com>
2024-03-11 15:09:25 -0300

    Implementação dos métodos delete e update na classe SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,10 @@
 package application;
 
-import java.util.List;
+import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,13 +13,17 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		// Seller seller = sellerDao.findById(6);
-		//Department depart = new Department(2, null);
+		Department depart = new Department(2, null);
 		//List<Seller> listSeller = sellerDao.findByDepartment(depart);
-		List<Seller> listSeller = sellerDao.findAll();
+		//List<Seller> listSeller = sellerDao.findAll();
 
-		for (Seller list : listSeller) {
-			System.out.println(list);
-		}
+		//for (Seller list : listSeller) {
+			//System.out.println(list);
+		//}
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, depart);
+		
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new id = " + newSeller.getId());
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -28,49 +28,79 @@ public class SellerDaoJDBC implements SellerDao {
 	public void insert(Seller obj) {
 		PreparedStatement pst = null;
 		try {
-			pst = conn.prepareStatement(
-					"INSERT INTO seller " 
-					+ "(Name, Email, BirthDay, BaseSalary, DepartmentId) "
-					+ "Values (?, ?, ?, ?, ?)",
-					Statement.RETURN_GENERATED_KEYS);
-			
+			pst = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDaTe, BaseSalary, DepartmentId) "
+					+ "Values (?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+
 			pst.setString(1, obj.getName());
 			pst.setString(2, obj.getEmail());
 			pst.setDate(3, new java.sql.Date(obj.getBirthdate().getTime()));
 			pst.setDouble(4, obj.getBaseSalary());
 			pst.setInt(5, obj.getDepartment().getId());
-			
+
 			int rowsAffected = pst.executeUpdate();
-			
-			if(rowsAffected > 0) {
+
+			if (rowsAffected > 0) {
 				ResultSet rs = pst.getGeneratedKeys();
-				if(rs.next()) {
+				if (rs.next()) {
 					int id = rs.getInt(1);
 					obj.setId(id);
 				}
 				DB.closeResultSet(rs);
-			}
-			else {
+			} else {
 				throw new DbException("Unexpected error! No rows affected!");
 			}
-			
+
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(pst);
 		}
 	}
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement pst = null;
+		
+		try {
+			pst = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET Name = ?, Email = ?, BithDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+"WHERE seller.id = ?");
+			pst.setString(1, obj.getName());
+			pst.setString(2, obj.getEmail());
+			pst.setDate(3, new java.sql.Date(obj.getBirthdate().getTime()));
+			pst.setDouble(4, obj.getBaseSalary());
+			pst.setInt(5, obj.getDepartment().getId());
+			pst.setInt(6, obj.getId());
+			
+			pst.executeUpdate();
+		
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(pst);
+		}
 
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement pst = null;
+		try {
+			pst = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+
+			pst.setInt(1, id);
+
+			int rowsAffected = pst.executeUpdate();
+
+			if (rowsAffected == 0) {
+				throw new DbException("No rows affected!");
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(pst);
+		}
 
 	}
 
